762107f74e72b40e7c33d690814605cc7f83f7d1
[subashiniMyapp] <[subashini.140525@gmail.com]>
2023-10-30 18:06:17 +0530

    poster

--- src/Moviesapp.jsx
+++ src/Moviesapp.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState,useEffect} from "react";
 import "./moives.css";
+import Modal from "./modal";
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const Imagpath = "https://image.tmdb.org/t/p/w1280";
@@ -57,14 +58,11 @@ function Moviesapp() {
       }
     },[search])
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    
 
-      const openModal = () => {
-        setIsModalOpen(true);
-      };
-
-      const closeModal = () => {
-        setIsModalOpen(false);
+      const getData = (img,title,rating,lan,overview) => {
+        let tempData = [img,title,rating,lan,overview];
+        console.log(tempData);
       };
 
   return (
@@ -85,7 +83,8 @@ function Moviesapp() {
               :(
               <div className="results">
               {movies.map((movie) => (
-                    <div className="movie-card" key={movie.id} onClick={openModal}> 
+                    <div className="movie-card" key={movie.id} 
+                    onClick={()=>getData(Imagpath+movie.poster_path,movie.title,movie.vote_average,movie.original_language,movie.overview) }> 
                        <img src={Imagpath+movie.poster_path} alt="Movie Poster" className="movie-poster" />
                         <h2 className="movie-title">{movie.title}</h2>
                         <h2 className="movie-rating">{movie.vote_average}</h2>
@@ -96,17 +95,8 @@ function Moviesapp() {
               )
 
           }
-    
+        <Modal/>
       </>
-      {isModalOpen && (
-        <div className="modal">
-            <h2>ibnjaln lsdngr</h2>
-            <h2>ibnjaln lsdngr</h2>
-            <h2>ibnjaln lsdngr</h2>
-            <h2>ibnjaln lsdngr</h2>
-          <button onClick={closeModal}>Close Modal</button>
-        </div>
-      )}
       <div className="footer">
         &copy; 2023 Movie Search App
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Moviesapp from "./Moviesapp";
+// import ModelApp from "./modal.jsx";
 // import WeatherComponet from "./WeatherComponet";
 ReactDOM.createRoot(document.getElementById("root")).render(<Moviesapp />);
 // root.render(<App />);
